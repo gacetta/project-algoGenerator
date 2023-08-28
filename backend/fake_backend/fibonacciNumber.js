d19354023850784bdfff7fd72fa885986f1d6ff4
gacetta <78240758+gacetta@users.noreply.github.com>
2023-08-28 10:24:48 -0700

    update: fibonacciNumber is now slow recursive

--- backend/fake_backend/fibonacciNumber.js
+++ backend/fake_backend/fibonacciNumber.js
@@ -4,27 +4,25 @@ const fibonacciNumber = {
   description:
     "Takes one argument of type number.  \nfibonacciNumber returns that number from the Fibonacci sequence.",
   args: ["number"],
-  funcBody: (num) => {
-    if (num === 0) return 0;
-    if (num === 1) return 1;
+  // funcBody: (num) => {
+  //   if (num === 0) return 0;
+  //   if (num === 1) return 1;
 
-    let a = 0;
-    let b = 1;
+  //   let a = 0;
+  //   let b = 1;
 
-    while (num > 0) {
-      [a, b] = [b, a + b];
-      num--;
-    }
+  //   while (num > 0) {
+  //     [a, b] = [b, a + b];
+  //     num--;
+  //   }
 
-    return a;
+  //   return a;
+  // },
+  funcBody: function (num) {
+    console.log("num:", num);
+    if (num <= 1) return num;
+    return this.funcBody(num - 1) + this.funcBody(num - 2);
   },
 };
 
 module.exports = fibonacciNumber;
-
-// slowfunc: (num) => {
-//   if(num<=1){
-//       return num;
-//   }
-//   return fib(num-1) + fib(num-2)
-// };
